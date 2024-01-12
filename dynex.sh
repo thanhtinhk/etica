98f3a931ab19b8115ef4967360b6d8a2f4003be9
thanhtinhk <152067750+thanhtinhk@users.noreply.github.com>
2024-01-12 19:32:42 +0700

    Add files via upload

---
+++ dynex.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./onezerominer -a dynex -w Xwoe3a8eoCeZ9kBPvu3dAW2JinMRvtNTRdHdwynfE43JQBLxtRrByzpTDgnM4X3qjsZrgfgojbnGEZRVepMAQYce28be1eDb3 -o dnx.eu.neuropool.net:19330,pool.deepminerz.com:3333 -p x
